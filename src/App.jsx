443ddb21b23ef253d8a40d725feda7234081da44
priyanka singh <piru.29072k@gmail.com>
2024-07-09 00:22:32 +0530

    without any reactjs code

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,14 @@
+// import React from "react";
 import "./App.css";
-// import { useState } from "react";
+import Home from "./components/Home.jsx";
 
 export default function App() {
-  // const [name, setName] = useState("");
-
-  // const handleSubmit = (event) => {
-  //   event.preventDefault();
-  //   console.log({ name });
-  //   alert(`The name you entered was: ${name}`);
-  // };
   return (
-    <>
-      <div>
-        <div className="container">
-          <form
-            // onSubmit={handleSubmit}
-            style={{ background: "white", width: "150%", height: "20vh" }}
-          >
-            <h2 className="text-center">Welcome to our website</h2>
-            <input
-              type="text"
-              name="name"
-              placeholder="Enter your name"
-              value={name}
-              // onChange={(e) => setName(e.target.value)}
-              className="form-control "
-            />
-            <button type="Submit" className="btn btn-dark offset-4 pt-2">
-              Submit
-            </button>
-          </form>
-        </div>
+    <div>
+      <h2>hi!..</h2>
+      <div className="container">
+        <Home />
       </div>
-    </>
+    </div>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,33 +1,20 @@
-import "./App.css";
-import { useState } from "react";
-export default function Home() {
-  const [name, setName] = useState("");
+// import React from 'react'
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log({ name });
-    alert(`The name you entered was: ${name}`);
-  };
+export default function Home() {
   return (
-    <>
-      <div className="container">
-        <form
-          onSubmit={handleSubmit}
-          style={{ background: "white", width: "150%", height: "20vh" }}
-        >
-          <input
-            type="text"
-            name="name"
-            placeholder="Enter your name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            className="form-control"
-          />
-          <button type="Submit" className="btn btn-dark">
-            Submit
-          </button>
-        </form>
-      </div>
-    </>
+    <div>
+      <form action="">
+        <input
+          type="email"
+          className="form-contol"
+          name="email"
+          placeholder="Enter your Email"
+          id=""
+        />
+        <button type="submit" className="btn btn-dark">
+          Submits
+        </button>
+      </form>
+    </div>
   );
 }
