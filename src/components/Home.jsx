ef33c0e2cf04def9a439f9452b1d27efb6aed6c4
priyanka singh <piru.29072k@gmail.com>
2024-07-09 01:40:38 +0530

    with api intereted

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 // import React from 'react'
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Home() {
   const [name, setName] = useState("");
@@ -8,7 +8,14 @@ export default function Home() {
     console.log(name);
     alert(`The name you entered was: ${name}`);
   };
-
+  useEffect(() => {
+    getData();
+  }, []);
+  async function getData() {
+    let Data = await fetch("http://127.0.0.1:8000/api/show");
+    let response = await Data.json();
+    console.log(response);
+  }
   return (
     <div>
       <form onSubmit={handleSubmit}>
