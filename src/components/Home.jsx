626b9420251edf9b77b166191e57cb3769bb2a80
priyanka singh <piru.29072k@gmail.com>
2024-07-09 00:29:18 +0530

    with reactjs basic codes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,25 @@
 // import React from 'react'
+import { useState } from "react";
 
 export default function Home() {
+  const [name, setName] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(name);
+    alert(`The name you entered was: ${name}`);
+  };
+
   return (
     <div>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <input
           type="email"
           className="form-contol"
           name="email"
           placeholder="Enter your Email"
           id=""
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
         <button type="submit" className="btn btn-dark">
           Submits
